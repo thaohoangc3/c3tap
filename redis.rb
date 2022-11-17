afa3f4b680719834879e0b865f878af158f38fac
thaohoangc3 <thaominhhoangvn@outlook.com>
2022-11-17 12:59:27 +0700

    add redis@5.0.9

---
+++ redis.rb
@@ -0,0 +1,72 @@
+class Redis < Formula
+  desc 'Persistent key-value database, with built-in net interface'
+  homepage 'https://redis.io/'
+  url 'http://download.redis.io/releases/redis-5.0.9.tar.gz'
+  sha256 '53d0ae164cd33536c3d4b720ae9a128ea6166ebf04ff1add3b85f1242090cb85'
+  head 'https://github.com/antirez/redis.git', branch: 'unstable'
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 '6dd79cce0a271a736f17a9c0e72fd8e5288b135426f5746d25496ab5048e0059' => :catalina
+    sha256 'c72def1c78f6c24c02d47de80a0b5ae5ef67640a42a38d040c503429eb02d3b1' => :mojave
+    sha256 '819c9292b32c8bdc90f51a7301cb44ffba8fa97fa8cf0db0e64e6ab5b0e41b9f' => :high_sierra
+  end
+
+  def install
+    # Architecture isn't detected correctly on 32bit Snow Leopard without help
+    ENV['OBJARCH'] = "-arch #{MacOS.preferred_arch}"
+
+    system 'make', 'install', "PREFIX=#{prefix}", "CC=#{ENV.cc}"
+
+    %w[run db/redis log].each { |p| (var / p).mkpath }
+
+    # Fix up default conf file to match our paths
+    inreplace 'redis.conf' do |s|
+      s.gsub! '/var/run/redis.pid', var / 'run/redis.pid'
+      s.gsub! 'dir ./', "dir #{var}/db/redis/"
+      s.sub!(/^bind .*$/, 'bind 127.0.0.1 ::1')
+    end
+
+    etc.install 'redis.conf'
+    etc.install 'sentinel.conf' => 'redis-sentinel.conf'
+  end
+
+  plist_options manual: "redis-server #{HOMEBREW_PREFIX}/etc/redis.conf"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+          </dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/redis-server</string>
+            <string>#{etc}/redis.conf</string>
+            <string>--daemonize no</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/redis.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/redis.log</string>
+        </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    system bin / 'redis-server', '--test-memory', '2'
+    %w[run db/redis log].each { |p| assert_predicate var / p, :exist?, "#{var / p} doesn't exist!" }
+  end
+end
